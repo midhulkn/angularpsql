64e4fa55839bbe9f879de50040341ec8b42c69c9
midhulkn <52281940+midhulkn@users.noreply.github.com>
2019-07-03 10:15:52 +0000

    Update updatePass.php

--- angularjs-postgres/updatePass.php
+++ angularjs-postgres/updatePass.php
@@ -8,7 +8,7 @@ $response = [];
 $dbconn = pg_connect("host=localhost port=8080 dbname=demodatabase user=postgres password=123456789");
 $newPass = pg_escape_string($dbconn, $_POST['newPass']);
 $query = "UPDATE users SET password = '$newPass' WHERE username = '".$_SESSION['user']."'";
-$result = pg_query($con, $query);
+$result = pg_query($dbconn, $query);
 if($result) {
 	$response['status'] = 'done';
 } else {
